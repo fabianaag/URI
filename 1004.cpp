397388db497512795efc87b76b7af82eed525075
falvesufcg <73008043+falvesufcg@users.noreply.github.com>
2021-10-29 12:08:37 -0300

    Create 1004.cpp

---
+++ 1004.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+main() {
+	
+int A, B, PROD;
+   
+   scanf("%i %i",&A,&B); 
+   PROD = A * B;                
+   printf("PROD = %i\n", PROD); 
+   
+   system("pause");
+	
+}
